106065de8222a90f5f32c3eb078ff74b24f85cb2
kevindu52defargoth <kevindu52defargoth@protonmail.com>
2023-02-16 08:27:43 +0100

    write_words

--- request.h
+++ request.h
@@ -6,3 +6,5 @@ struct XwayAddr {
 void affiche_trame(char * buff);
 
 void send_trame(int socket, char * msg, int lenMsg, struct XwayAddr src, struct XwayAddr dest, char * response, int * lenResponse);
+
+void write_words(int socket, char * msg, int nbreMot, struct XwayAddr src, struct XwayAddr dest, char * response, int * lenResponse);
