be188ef412d45715164ec712ef60634231761302
kevindu52defargoth <kevindu52defargoth@protonmail.com>
2023-03-09 12:04:18 +0100

    😠

--- request.c
+++ request.c
@@ -189,7 +189,7 @@ void wait_ressource(int socket, struct XwayAddr src, struct XwayAddr dest) {
   affiche_trame(buff_rx);
 #endif
   //printf("%d\n", nbcar);
-  if (buff_rx[12] == 0x4b) {
+  if (buff_rx[14] == 0x4b) {
     printf("ressource prise\n");
     att = 1;
   }
